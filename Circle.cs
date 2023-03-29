587821c62a48bebaf23074f93d8e02a08881f23b
imtusharganta <71046284+imtusharganta@users.noreply.github.com>
2023-03-29 14:32:58 -0700

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+      void Start()
+    {
+        Debug.Log("Starting Circle: " + gameObject.name);
+    }
+
+    public float speed = 5f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}

615be427ce37ac21b34fae3bfe6eefa93f7cdec6
TrentLucas <72095714+TrentLucas@users.noreply.github.com>
2023-03-28 23:39:36 -0700

    Added lucast24 scene
    Has a parent circle with child objects that rotate around the parent circle in real-time.

---
+++ Assets/MoveCircle3.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCircle3 : MonoBehaviour
+{
+    private float rotationSpeed = 3f;
+    private float radius = 3f;
+
+    public Transform Target;
+
+    private float angle;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 targetPos = Target.position;
+
+        angle += rotationSpeed * Time.deltaTime;
+
+        pos.x = targetPos.x - (Mathf.Sin(angle) * radius);
+
+        pos.y = targetPos.y + (Mathf.Cos(angle) * radius);
+
+        transform.position = pos;
+    }
+}
